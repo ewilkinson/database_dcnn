27f02d6b14f5da509612a418f5aa3f33e21f7c79
ttakahas <takeshi@penguin.(none)>
2015-04-02 16:53:40 -0400

    added store_feature_into_db.py

---
+++ code/store_feature_into_db.py
@@ -0,0 +1,47 @@
+import psycopg2
+import numpy as np
+import os
+
+
+conn = psycopg2.connect(dbname="mydb", user="takeshi", password="asdfgh", host="127.0.0.1")
+
+cur = conn.cursor()
+
+#cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+#cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(100,"abc'def"))
+
+cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data float8[]);")
+#cur.execute("SELECT * FROM test;")
+
+
+#------------------------ Script Parameters ---------------------
+
+feature_layers = ['conv3', 'conv4', 'fc6', 'fc7', 'pool1', 'pool2', 'pool5']
+feature_dir = "../features"
+feature_files = os.listdir(feature_dir)
+num_files_to_use = 10  #max = 50000
+count = 0
+for layer_dir in feature_files:
+    feature_files = np.sort(os.listdir(feature_dir+'/'+layer_dir))
+    if layer_dir == 'pool5':
+        for file in feature_files:
+            years, types, img_ids = [], [], []
+            images = []
+            print file
+            feature = np.load(feature_dir+'/'+layer_dir+'/'+file)
+            feature2 = feature.ravel().tolist()
+            cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(count,feature2))
+            cur.execute("SELECT data FROM test;")
+            #feature3 =cur.fetchone()
+            #print feature3
+            count = count + 1
+            if count == num_files_to_use:
+                break
+ 
+         
+ 
+#print cur.fetchone()
+
+conn.commit()
+cur.close()
+conn.close()
